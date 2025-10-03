5ea939cb97ce77576f33530a335a0cfd8d54ab3f
prajit <prajitkumardas01@gmail.com>
2025-10-03 23:33:44 +0530

    Third Push

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: E:\crm_react\app\layout.js
+// File: e:\crm_react\app\layout.js
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
